176a028fb605716cac7ce363f6fb1fbc4071fc15
mickesv <Mikael Svahnberg@gmail.com>
2022-09-12 14:14:40 +0200

    keepalive monitor

--- Containers/QFWorker/src/index.js
+++ Containers/QFWorker/src/index.js
@@ -17,12 +17,22 @@ if (!process.env.REDIS_HOST) {
     console.log('WARNING: the environment variable REDIS_HOST is not set');
 }
 
+var worker = new RSMQWorker(SEARCHQUEUE, {host: process.env.REDIS_HOST});
+
+// Timeout watchdog
+// --------------------
+const HBINTERVAL = 60000;
+function keepAlive() {
+    console.log('Connected to queue', SEARCHQUEUE, ':', worker.queue.connected);
+    setTimeout(keepAlive, HBINTERVAL);
+}
+keepAlive() // Comment this line to remove these status messages.
+
 // Start listening for jobs on SEARCHQUEUE
 // ========================================
-var worker = new RSMQWorker(SEARCHQUEUE, {host: process.env.REDIS_HOST});
 worker.on('message', (msg, next, id) => {
-    console.log('Job on queue', SEARCHQUEUE, id, msg);
     let job = JSON.parse(msg);
+    console.log(SEARCHQUEUE,job.searchString, 'in Text', job.textTitle);
     let textManager = new TextManager();
     let textSearcher = new ActiveSearchStrategy();
     // textSearcher.testSearch();
@@ -32,13 +42,13 @@ worker.on('message', (msg, next, id) => {
         .then( () => textManager.startSearch(job, MAXTHREADS, textSearcher))
         .then( result => result.flat() )
         .then ( result => {
-            console.log('Back in dispatch, preparing to return results', result.length);
+            console.log('Number of results:', result.length);
             result.forEach(r => {
                 let cleanedResult = r.replace(/[\n\r]/g, ' ').trim();
                 rsmq.sendMessage( {qname: job.returnQueue, message: JSON.stringify({ textTitle: job.textTitle,
                                                                                      contents: cleanedResult })})})}) // This is starting to look a lot like lisp, don't you think?
-        .catch( err => console.log('Failed during search', err))
         .then( () => rsmq.sendMessage( {qname: job.returnQueue, message: 'DONE'}))
+        .catch( err => console.log('Failed during search. Continuing...'))
         .then( () => next());
 });
 
